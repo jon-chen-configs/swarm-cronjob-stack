4a10f4015a68cc588bca0551a728920b5fe77c8f
Jonathan Chen <jonchen228@gmail.com>
2022-09-15 13:12:13 -0400

    first commit

---
+++ swarm-cronjob.deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+WD=$(dirname $0)
+SCRIPTS_DIR=${WD}/../../scripts
+export ENV_DIR=${WD}/../../../compose/conf/swarm-env-config
+export ENV_FILE=${ENV_DIR}/swarm.env
+export COMPOSE_FILE=${WD}/swarm-cronjob-swarm.yml
+export STACK_NAME=swarm-cronjob-swarm 
+
+${SCRIPTS_DIR}/stack.deploy.sh
