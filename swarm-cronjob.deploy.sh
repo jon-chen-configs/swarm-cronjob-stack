1ff1b594c6757972b23311cc068c2305be6dc3ac
Jonathan Chen <jonchen228@gmail.com>
2022-11-14 21:13:27 -0500

    move config to subdirectory.

--- swarm-cronjob.deploy.sh
+++ swarm-cronjob.deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 WD=$(dirname $0)
-SCRIPTS_DIR=${WD}/../../scripts
-export ENV_DIR=${WD}/../../../compose/conf/swarm-env-config
+SCRIPTS_DIR=${WD}/../../../scripts
+export ENV_DIR=${WD}/../../../../compose/conf/swarm-env-config
 export ENV_FILE=${ENV_DIR}/swarm.env
 export COMPOSE_FILE=${WD}/swarm-cronjob-swarm.yml
 export STACK_NAME=swarm-cronjob-swarm 
--- swarm-cronjob.remove.sh
+++ swarm-cronjob.remove.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WD=$(dirname $0)
-SCRIPTS_DIR=${WD}/../../scripts
+SCRIPTS_DIR=${WD}/../../../scripts
 export STACK_NAME=swarm-cronjob-swarm
 
 ${SCRIPTS_DIR}/stack.remove.sh
